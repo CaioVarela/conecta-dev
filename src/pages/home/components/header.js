1f6d9883bb496e53fa9bf2756c9bcca85ddc0ad5
CaioVarela <caiovareladelima@gmail.com>
2022-03-25 07:55:25 -0300

    initial code

---
+++ src/pages/home/components/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header(){
+    return (
+        <header className="header">
+                <div className="toolbar">
+                    <div className="">
+                        <span>Conecta Dev</span>
+                    </div>
+                    <div className="">
+                        <button>Novo Post</button>
+                        <span>img 1</span>
+                        <span>img 2</span>
+                    </div>
+                </div>
+            </header>
+    );
+}
+export default Header;
